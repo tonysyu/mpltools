127494eda6d4938fa8d1aa4ac15736373c79eedb
Tony S Yu <tsyu80@gmail.com>
2012-02-16 21:45:46 -0500

    DOC: Add Mock Modules to try and build on ReadTheDocs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -246,3 +246,27 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+# -- Mock modules for building with ReadTheDocs.org
+
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
