1851ac615319b95f202b683944b7ce89c10b4e7e
Tony S Yu <tsyu80@gmail.com>
2012-02-18 13:54:04 -0500

    DOC: Simplify example plot.

--- doc/source/auto_examples/plot_color_mapper.py
+++ doc/source/auto_examples/plot_color_mapper.py
@@ -17,10 +17,9 @@ from mpltools import color
 
 pvalues = np.logspace(-1, 0, 4)
 parameter_range = (pvalues[0], pvalues[-1])
-cstart = 0.2
 # Pass parameter range so that color mapper knows how to normalize the data.
 map_color1 = color.color_mapper(parameter_range)
-map_color2 = color.color_mapper(parameter_range, cmap='BuPu', start=cstart)
+map_color2 = color.color_mapper(parameter_range, cmap='BuPu', start=0.2)
 
 figsize = mpltools.figsize(aspect_ratio=2)
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=figsize)
@@ -30,15 +29,10 @@ for pval in pvalues:
     ax1.plot(x, y, 's', color=map_color1(pval))
     ax2.plot(x, y, lw=3, color=map_color2(pval))
 
-ax1.legend(['%0.1f' % v for v in pvalues], loc='lower right', ncol=2)
-ax1.set_ylim(-1.5, 1.5)
-
-# Hack to get correctly-scaled colorbar
-proxy_mappable = plt.cm.ScalarMappable(cmap='BuPu')
-map_start = parameter_range[1] - np.diff(parameter_range) / (1. - cstart)
-proxy_mappable.set_array(parameter_range)
-cbar = fig.colorbar(proxy_mappable, ax=ax2)
-cbar.set_clim((map_start, parameter_range[1]))
+for ax in (ax1, ax2):
+    leg = ax.legend(['%0.1f' % v for v in pvalues], loc='lower right', ncol=2)
+    leg.set_title('decay rate')
+    ax.set_ylim(-1.5, 1)
 
 plt.show()
 
