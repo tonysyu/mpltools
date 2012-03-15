7f8e4950e60efcd73fedb8559ddd712511334887
Tony S Yu <tsyu80@gmail.com>
2012-03-15 10:41:54 -0400

    Add Animation class.

---
+++ examples/animation_examples.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpltools.animation import Animation
+
+
+# TODO: The first plotted line remains for the duration of the animation.
+class DecayingOscillations(Animation):
+
+    def __init__(self, L=2*np.pi, npts=100, noscillations=5, decay_rate=0.1):
+        self.fig, self.ax = plt.subplots()
+        self.x = np.linspace(0, 2*np.pi, npts)
+        self.noscillations = noscillations
+        self.decay_rate = decay_rate
+        self.line, = self.ax.plot(self.x, np.sin(self.x))
+
+    def update(self):
+        tmax = self.noscillations * 2*np.pi
+        for t in np.linspace(0, tmax, 100):
+            amplitude = np.exp(-t * self.decay_rate) * np.cos(t)
+            self.line.set_ydata(amplitude * np.sin(self.x))
+            yield self.line, # must return artist if you want to use blit
+
+osc = DecayingOscillations()
+osc.animate(blit=True)
+
+plt.show()
+
---
+++ mpltools/animation.py
@@ -0,0 +1,126 @@
+"""
+Animation class.
+
+This implementation is a interface for Matplotlib's FuncAnimation class, but
+with different interface for:
+
+* Easy reuse of animation code.
+
+* Logical separation of setup parameter (passed to `__init__`) and animation
+  parameters (passed to `animate`).
+
+* Unlike Matplotlib's animation class, this Animation class clearly must be
+  assigned to a variable (in order to call the `animate` method). The
+  FuncAnimation object needs to be assigned to a variable so that it isn't
+  garbage-collected, but this requirement is confusing, and easily forgotten,
+  because the user never uses the animation object directly.
+
+
+"""
+from matplotlib.animation import FuncAnimation, TimedAnimation
+
+
+__all__ = ['Animation']
+
+
+class Animation(object):
+    """Base class to create animation objects.
+
+    To create an animation, simply subclass `Animation` and override the
+    `__init__` method to create a plot (`self.fig` needs to be assigned to the
+    figure object here), and override `update` with a generator that updates
+    the plot:
+
+    .. code-block:: python
+
+       class RandomPoints(Animation):
+
+           def __init__(self, width=10):
+               self.fig, self.ax = plt.subplots()
+               self.width = width
+               self.ax.axis([0, width, 0, width])
+
+           def update(self):
+               artists = []
+               self.ax.lines = [] # Clean up plot when repeating animation.
+               for i in np.arange(20):
+                   x, y = np.random.uniform(0, self.width, size=2)
+                   artists.append(self.ax.plot(x, y, 'ro'))
+                   yield artists
+
+       pts = RandomPoints()
+       pts.animate()
+
+    Note: if you want to use blitting (see docstring for `Animation.animate`),
+    You must yield a sequence of artists in `update`.
+
+    This Animation class does not subclass any of Matplotlib's animation
+    classes because the `__init__` method takes arguments for creating the
+    plot, while `animate` method is what accepts arguments that alter the
+    animation.
+
+    """
+    def __init__(self):
+        """Initialize plot for animation.
+
+        Replace this method to initialize the plot. The only requirement is
+        that you must create a figure object assigned to `self.fig`.
+        """
+        raise NotImplementedError
+
+    def update(self):
+        """Update frame.
+
+        Replace this method to with a generator that updates artists and calls
+        an empty `yield` when updates are complete.
+        """
+        raise NotImplementedError
+
+    def animate(self, **kwargs):
+        """Run animation.
+
+        Parameters
+        ----------
+        interval : float, defaults to 200
+            Time delay, in milliseconds, between frames.
+
+        repeat : {True | False}
+            If True, repeat animation when the sequence of frames is completed.
+
+        repeat_delay : None
+            Delay in milliseconds before repeating the animation.
+
+        blit : {False | True}
+            If True, use blitting to optimize drawing. Unsupported by some
+            backends.
+
+        init_func : function
+            Function to draw a clear frame. If None, the results of drawing from
+            the first item in the frames sequence will be used.
+
+        save_count : int
+            If saving a movie, `save_count` determines number of frames saved.
+
+        """
+        reusable_generator = lambda: iter(self.update())
+        self._ani = GenAnimation(self.fig, reusable_generator, **kwargs)
+
+
+class GenAnimation(FuncAnimation):
+
+    def __init__(self, fig, frames, init_func=None, save_count=None,
+                 **kwargs):
+        self._iter_gen = frames
+
+        self._init_func = init_func
+        self.save_count = save_count if save_count is not None else 100
+
+        # Dummy args and function for compatibility with FuncAnimation
+        self._args = ()
+        self._func = lambda args: args
+
+        self._save_seq = []
+        TimedAnimation.__init__(self, fig, **kwargs)
+        # Clear saved seq since TimedAnimation.__init__ adds a single frame.
+        self._save_seq = []
+
