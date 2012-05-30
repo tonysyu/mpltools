a8470ab5c3856b805d038076de7a7ff75bf6860e
Alex Arsenovic <arsenovic@virginia.edu>
2012-05-30 08:25:07 -0400

    added suggestions from tony

--- mpltools/io/util.py
+++ mpltools/io/util.py
@@ -1,30 +1,38 @@
 import matplotlib.pyplot as plt
+import os
 
-def save_all_figs(dir = './', format=['eps','pdf','png']):
+def save_all_figs(directory = './', format=['eps','pdf','png'], 
+    default_name = 'untitled%i'):
     """
     Save all open Figures to a given directory, possbily in numerous 
     file formats
     
     Parameters
     ------------
-        dir : string
-            path to save figures into
-        format : list of strings
-            the types of formats to save figures as. The elements of this
-            list are passed to :matplotlib:`savefig`. 
-            
+    directory : str
+        path to save figures into
+    format : list of strs
+        the types of formats to save figures as. The elements of this
+        list are passed to matplotlib's `savefig`. 
+    default_name : str
+        the default filename to use if plot has no title. must contain 
+        a '%i' for the figure number
+    
     Examples
     --------
     >>> save_all_figs('plots/', ['pdf','png'])
     
     """
-    if dir[-1] != '/':
-        dir = dir + '/'
     
     for fignum in plt.get_fignums():
-        fileName = plt.figure(fignum).get_axes()[0].get_title()
-        if fileName == '':
-            fileName = 'unamedPlot'
+        filename = plt.figure(fignum).get_axes()[0].get_title()
+        
+        if filename == '':
+            filename = default_name % fignum
+                
+        savename = os.path.join(directory, filename)
+
         for fmt in format:
-            plt.savefig(dir+fileName+'.'+fmt, format=fmt)
-            print (dir+fileName+'.'+fmt)
+            plt.savefig(savename + '.' + fmt)
+            print ('Saved \'%s\' '% (savename + '.' + fmt))
+
