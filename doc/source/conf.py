f9d963002ee20b3b979dd760e1a2f4f9c4890334
Tony S Yu <tsyu80@gmail.com>
2012-07-08 18:11:15 -0400

    Add logo and new theme.
    Theme is derived from the 'pyramid' theme that comes with Sphinx. The theme was copied just so the logo could be placed in the sidebar instead of at the top.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -97,7 +97,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid'
+html_theme = 'mpltools'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,18 +105,18 @@ html_theme = 'pyramid'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['../theme/']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'mpltools docs'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/mpltools_logo_100.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
