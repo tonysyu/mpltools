b89b3086297eb3aaee9ae1c604aba1368cd04102
Tony S Yu <tsyu80@gmail.com>
2012-07-09 00:31:24 -0400

    Fix packaging to prepare for v0.1 release.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python
-
 from distutils.core import setup
 
+import setuptools
+
+
 setup(name='mpltools',
-      version='0.1dev',
+      version='0.1',
       description='Tools for Matplotlib',
       author='Tony S. Yu',
       author_email='tsyu80@gmail.com',
       license='Modified BSD',
+      url='http://tonysyu.github.com/mpltools/',
       download_url='http://github.com/tonysyu/mpltools',
-      packages=['mpltools','mpltools.style','mpltools.widgets','mpltools.special'],
+      packages=setuptools.find_packages(),
+      package_data={'mpltools': ['mpltoolsrc', 'style/*.rc']},
      )
