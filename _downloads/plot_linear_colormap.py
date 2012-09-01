ea5d94308b0fd474d8a4b707b56840fda4431ed7
Tony S Yu <tsyu80@gmail.com>
2012-09-01 21:47:00 +0500

    Update documentation

--- _downloads/plot_linear_colormap.py
+++ _downloads/plot_linear_colormap.py
@@ -3,11 +3,11 @@
 LinearColormap
 ==============
 
-This class simplifies the creation of Matplotlib. To specify a colormap, you
-can just specify key colors in the colormap, and ``LinearColormap`` will
-distribute those colors evenly in the colormap and linearly interpolate
-in-between. In the example below, specifying two colors defines the minimum and
-maximum color values of the colormap.
+This class simplifies the creation of Matplotlib colormaps. To specify
+a colormap, you can just specify key colors in the colormap, and
+``LinearColormap`` will distribute those colors evenly in the colormap and
+linearly interpolate in-between. In the example below, specifying two colors
+defines the minimum and maximum color values of the colormap.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,9 +17,7 @@ from mpltools import color
 
 x, y, z = np.random.uniform(size=(3, 100))
 
-white_red = color.LinearColormap('white_red', {'red': [1, 0.8],
-                                               'green': [1, 0],
-                                               'blue': [1, 0]})
+white_red = color.LinearColormap('white_red', [(1, 1, 1), (0.8, 0, 0)])
 plt.scatter(x, y, c=z, cmap=white_red, s=200)
 
 """
@@ -31,11 +29,11 @@ colormap. This colormap is uniformly blue below the midpoint and red above the
 midpoint. Alpha values are maximum at the edges and minimum in the middle.
 """
 
-bcr_spec = {'blue': [0.4, 0.4, 0.1, 0.1],
-           'green': [0.2, 0.2, 0.0, 0.0],
-           'red': [0.02, 0.02, 0.4, 0.4],
-           'alpha': [1, 0.3, 0.3, 1]}
-blue_clear_red = color.LinearColormap('blue_clear_red', bcr_spec,
+bcr_rgba = [(0.02, 0.2, 0.4, 1),    # grayish blue, opaque
+            (0.02, 0.2, 0.4, 0.3),  # grayish blue, transparent
+            (0.4,  0.0, 0.1, 0.3),  # dark red, transparent
+            (0.4,  0.0, 0.1, 1)]    # dark red, opaque
+blue_clear_red = color.LinearColormap('blue_clear_red', bcr_rgba,
                                       index=[0, 0.5, 0.5, 1])
 
 
